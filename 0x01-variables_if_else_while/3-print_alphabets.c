2e3aec1031ed1c463d345c6bd97c00104c72532a
SSGOVOS <moubine41@gmail.com>
2023-03-17 23:51:00 +0100

    updated

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main-program entry point.
+ *
+ *Return:0 if no error, non-zero value if error.
+ */
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		putchar(c);
+	}
+	{
+		putchar(C);
+	}
+		putchar('\n');
+		return 0;
+	}
