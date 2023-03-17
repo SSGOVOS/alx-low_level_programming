bcf0b644a745222ca8ac9cfda462678f244673cb
SSGOVOS <moubine41@gmail.com>
2023-03-17 22:34:29 +0100

    updated

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,7 +9,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	int last_digit = abs(n) % 10;
+	int last_digit = n % 10;
 	printf("The last digit of %d is %d\n", n, last_digit);
 	return (0);
 }
