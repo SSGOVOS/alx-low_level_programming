2dd4c3cc6e1b8e3e550714e6fb1427f47592b1d0
SSGOVOS <moubine41@gmail.com>
2023-03-18 00:32:47 +0100

    updated

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * program prints all single digit numbers of base 10 starting from 0
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 122; i >=97; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return(0);
+}
