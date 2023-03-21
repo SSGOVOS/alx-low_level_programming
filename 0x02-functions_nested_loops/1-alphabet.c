bb4ad770190169e7c1eb162d107f0691c309bf5c
SSGOVOS <moubine41@gmail.com>
2023-03-21 23:27:18 +0000

    updated

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 /**
  * print_alphabet - Entry point ,this prints alphabet
  * in lowercase
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 /**
  * _islower - checks if input alphabet(in this example
  * letter c).
