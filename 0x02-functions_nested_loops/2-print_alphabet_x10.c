2037cd20dc5bc6328f31e3e21f641b4771f44bf9
SSGOVOS <moubine41@gmail.com>
2023-03-21 22:57:31 +0000

    updated

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,10 +10,10 @@ void print_alphabet_x10(void)
     for (i = 0; i < 10; i++) {
         letter = 'a';
         while (letter <= 'z') {
-            _putchar(letter);
+            putchar(letter);
             letter++;
         }
-        _putchar('\n');
+        putchar('\n');
     }
 }
 
