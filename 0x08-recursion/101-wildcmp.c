28d3bf019f90275f60f5ea518fc353b2d992cc1b
SSGOVOS <moubine41@gmail.com>
2023-04-05 22:22:18 +0000

    updated

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
 *wildcmp - compares two strings and returns 1 if they can be considered identical
-*@s1: first string to compare
-*@s2: second string to compare (can contain wildcard character *)
-*Return: 1 if the strings can be considered identical, otherwise return 0.
+*@s1:first string to compare
+*@s2:second string to compare(can contain wildcard character *)
+*Return:1 if the strings can be considered identical, otherwise return 0.
 */
 int wildcmp(char *s1, char *s2)
 {
